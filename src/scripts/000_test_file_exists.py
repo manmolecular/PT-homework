88877da61b912e79a88b6e495cbbbeb58e77334a
user <regwebghost@yandex.ru>
2018-05-02 23:11:39 +0700

    rebase test of existence with readable values

--- src/scripts/000_test_file_exists.py
+++ src/scripts/000_test_file_exists.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 # First test - check file existense
-from enum import Enum
 from db_handling import *
 from transports import *
 
 _file_name = 'testfile'
 _transport_name = 'SSH'
 
-class Status(Enum):
-    COMPLIANT = 1
-    NOT_COMPLIANT = 2
-    NOT_APPLICABLE = 3
-    ERROR = 4
-    EXCEPTION = 5
-
 def main():
     try:
         func_status = get_transport(_transport_name).is_exist(_file_name)
     except TransportUnknown:
-        return Status.ERROR.value
+        return Status.STATUS_ERROR.value
     if not func_status:
-        return Status.NOT_COMPLIANT.value
+        return Status.STATUS_NOT_COMPLIANT.value
     if func_status:
-        return Status.COMPLIANT.value
+        return Status.STATUS_COMPLIANT.value
