4e9b20cbd1c9592970863f1756982029213eb189
user <regwebghost@yandex.ru>
2018-05-02 19:25:01 +0700

    improve work with pdf report

--- src/report.py
+++ src/report.py
@@ -2,18 +2,36 @@
 # Module for report creating
 from jinja2 import Environment, PackageLoader, FileSystemLoader, select_autoescape
 from collections import namedtuple
+from weasyprint import HTML, CSS
+from db_handling import *
+
+report_data = []
+record = namedtuple('record','filename, header, script_id, descr, requirement, status')
 
 env = Environment(
     loader = FileSystemLoader('templates'),
     autoescape = select_autoescape(['html', 'xml'])
 )
 
-user_list = ['user {}'.format(n) for n in range(0, 10)]
-print(user_list)
-record = namedtuple('record','filename, script_id, descr, transports, status')
-report_data = [record(filename=name,script_id='test',descr='test',transports='test',status='test') for name in user_list]
+def get_rendered_html():
+    db = get_db_obj()
+    curr = db.cursor()
+    scandata_ids = curr.execute("SELECT id FROM scandata").fetchall()
+    for scan in scandata_ids:
+        scan_id = scan[0]
+        scan_header = curr.execute("SELECT header FROM control WHERE id = ?", str(scan[0])).fetchone()[0]
+        scan_descr = curr.execute("SELECT descr FROM scandata WHERE id = ?", str(scan[0])).fetchone()[0]
+        scan_status = curr.execute("SELECT status FROM scandata WHERE id = ?", str(scan[0])).fetchone()[0]
+        scan_filename = curr.execute("SELECT filename FROM control WHERE id = ?", str(scan[0])).fetchone()[0]
+        scan_requirements = curr.execute("SELECT requirement FROM control WHERE id = ?", str(scan[0])).fetchone()[0]
 
-test_tpl = env.get_template('index.html')
-rendered_html = test_tpl.render(report_data = report_data)
+        report_data.append(record(filename = scan_filename, header = scan_header, script_id = scan_id, 
+            descr = scan_descr, requirement = scan_requirements, status = scan_status))
 
-print(rendered_html)
+    template = env.get_template('index.html')
+    return template.render(report_data = report_data)
+
+def make_report():
+    whtml = HTML(string=get_rendered_html().encode('utf8'))
+    wcss = CSS(filename='./templates/style.css')
+    whtml.write_pdf('sample_report.pdf',stylesheets=[wcss])
