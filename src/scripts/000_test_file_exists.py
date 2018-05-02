86d64d0f7d76570f0fea516d8bf146ca13e4aa8b
user <regwebghost@yandex.ru>
2018-05-02 22:44:22 +0700

    turn magic numbers from test to enum

--- src/scripts/000_test_file_exists.py
+++ src/scripts/000_test_file_exists.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 # First test - check file existense
+from enum import Enum
 from db_handling import *
 from transports import *
 
 _file_name = 'testfile'
 _transport_name = 'SSH'
 
+class Status(Enum):
+    COMPLIANT = 1
+    NOT_COMPLIANT = 2
+    NOT_APPLICABLE = 3
+    ERROR = 4
+    EXCEPTION = 5
+
 def main():
     try:
-        get_transport(_transport_name).is_exist(_file_name)
+        func_status = get_transport(_transport_name).is_exist(_file_name)
     except TransportUnknown:
-        return 4
-    except TransportIOError:
-        return 2
-    return 1
+        return Status.ERROR.value
+    if not func_status:
+        return Status.NOT_COMPLIANT.value
+    if func_status:
+        return Status.COMPLIANT.value
