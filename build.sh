e43b6721866fb6dee5d1737b25ab67ee92e50478
user <regwebghost@yandex.ru>
2018-05-03 22:57:39 +0700

    remove sudo for docker command

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Build and run ssh docker
-set -e
-
-cd ./img-ubuntu-sshd/ && sudo docker build . -t img-ubuntu-sshd
-sudo docker run -d -p 22022:22 --name cont-ubuntu-sshd img-ubuntu-sshd
-ssh-keygen -f "/home/user/.ssh/known_hosts" -R [localhost]:22022
