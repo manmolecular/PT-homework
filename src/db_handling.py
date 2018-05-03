f158c4f4d00c53add304ab27c6fb1f65b9185848
user <regwebghost@yandex.ru>
2018-05-03 20:36:28 +0700

    connection as a context manager

--- src/db_handling.py
+++ src/db_handling.py
@@ -9,6 +9,11 @@ _json_db = None
 _db_contest = 'configs/controls.json'
 _db_name = 'database.db'
 
+class DatabaseError(sqlite3.Error):
+    def __init__(self, error_args):
+        Exception.__init__(self, 'DatabaseError {}'.format(error_args))
+        self.error_args = error_args
+
 class Status(Enum):
     STATUS_COMPLIANT = 1
     STATUS_NOT_COMPLIANT = 2
@@ -16,42 +21,56 @@ class Status(Enum):
     STATUS_ERROR = 4
     STATUS_EXCEPTION = 5
 
-def get_db_obj():
-    db = sqlite3.connect(_db_name)
-    return db
+def connect_database():
+    connection = sqlite3.connect(_db_name)
+    return connection
 
 def get_full_path():
     my_path = os.path.abspath(os.path.dirname(__file__))
     return os.path.join(my_path, _db_contest)
 
-def load_db():
+def load_json():
     global _json_db
     if not _json_db:
         with open(get_full_path(),'r') as f:
             _json_db = json.load(f)
     return _json_db
-
+     
 def create_db():
-    db = get_db_obj()
-    curr = db.cursor()
-    curr.execute('''
-        CREATE TABLE IF NOT EXISTS control(id INTEGER PRIMARY KEY, header TEXT, descr TEXT, filename TEXT, requirement TEXT)
-        ''')
-    curr.execute('''
-        CREATE TABLE IF NOT EXISTS scandata(id INTEGER PRIMARY KEY, descr TEXT, status TEXT)
-        ''')
-    controls = load_db()
+    connection = connect_database()
+    curr = connection.cursor()
+
+    try:
+        with connection:
+            curr.execute('''
+                CREATE TABLE IF NOT EXISTS control(id INTEGER PRIMARY KEY, header TEXT, descr TEXT, filename TEXT, requirement TEXT)
+                ''')
+            curr.execute('''
+                CREATE TABLE IF NOT EXISTS scandata(id INTEGER PRIMARY KEY, descr TEXT, status TEXT)
+                ''')
+    except sqlite3.Error as e:
+        raise DatabaseError(e.args[0])
+
+    controls = load_json()
     for cur_control in controls:
-        curr.execute("INSERT OR REPLACE INTO control(id, header, descr, filename, requirement) VALUES(?, ?, ?, ?, ?)", 
-            (cur_control[0], cur_control[1], cur_control[2], cur_control[3], cur_control[4]))
-    db.commit()
-    db.close()
+        try:
+            with connection:
+                curr.execute("INSERT OR REPLACE INTO control(id, header, descr, filename, requirement) VALUES(?, ?, ?, ?, ?)", 
+                    (cur_control[0], cur_control[1], cur_control[2], cur_control[3], cur_control[4]))
+        except sqlite3.Error as e:
+            raise DatabaseError(e.args[0])
+
+    connection.close()
 
 def add_control(control_id, status):
-    db = get_db_obj()
-    curr = db.cursor()
-    descr = (curr.execute("SELECT descr FROM control WHERE id = ?", str(control_id)).fetchone())[0]
-    curr.execute("INSERT OR REPLACE INTO scandata(id, descr, status) VALUES(?, ?, ?)", 
-            (control_id, descr, Status(status).name))
-    db.commit()
-    db.close()
+    connection = connect_database()
+    curr = connection.cursor()
+    try:
+        with connection:
+            descr = (curr.execute("SELECT descr FROM control WHERE id = ?", str(control_id)).fetchone())[0]
+            curr.execute("INSERT OR REPLACE INTO scandata(id, descr, status) VALUES(?, ?, ?)", 
+                    (control_id, descr, Status(status).name))
+    except sqlite3.Error as e:
+        raise DatabaseError(e.args[0])
+
+    connection.close()
