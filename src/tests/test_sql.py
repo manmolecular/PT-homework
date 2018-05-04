92cc28bcc8c56f2820f860c3cf2eda60f040c964
user <regwebghost@yandex.ru>
2018-05-04 23:55:25 +0700

    new tests

---
+++ src/tests/test_sql.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# Tests for sql transport
+from transports import MySQLtransport, get_defaults, get_transport
+from transports import TransportError, TransportUnknown, TransportConnectionError, TransportIOError
+import pymysql
+import pytest
+
+SQLdefaults = get_defaults('SQL')
+sql_data = 'webmaster@python.org'
+
+def prepare_base():
+    connection = pymysql.connect(host = 'localhost', 
+            user = 'root', 
+            port = 43306, 
+            password = 'password', 
+            db = 'def_database', 
+            charset='utf8', 
+            cursorclass=pymysql.cursors.DictCursor, 
+            unix_socket=False)
+
+    with connection.cursor() as cursor:
+        cursor.execute('''
+            DROP TABLE IF EXISTS users;
+            ''')
+
+    with connection.cursor() as cursor:
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS `users` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `email` varchar(255) COLLATE utf8_bin NOT NULL,
+        `password` varchar(255) COLLATE utf8_bin NOT NULL,
+        PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+            AUTO_INCREMENT=1 ;''')
+
+    with connection.cursor() as cursor:
+        sql = "INSERT IGNORE INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    connection.commit()
+    connection.close()
+
+def drop_n_create_table():
+    connection = pymysql.connect(host = 'localhost', 
+        user = 'root', 
+        port = 43306, 
+        password = 'password', 
+        db = 'def_database', 
+        charset='utf8', 
+        cursorclass=pymysql.cursors.DictCursor, 
+        unix_socket=False)
+
+    with connection.cursor() as cursor:
+        cursor.execute('''
+            DROP TABLE IF EXISTS users;
+            ''')
+   
+    with connection.cursor() as cursor:
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS `users` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `email` varchar(255) COLLATE utf8_bin NOT NULL,
+        `password` varchar(255) COLLATE utf8_bin NOT NULL,
+        PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+            AUTO_INCREMENT=1 ;''')
+
+    connection.commit()
+    connection.close()
+
+def test_mysql_init():
+    sql_transport = MySQLtransport(SQLdefaults['host'], SQLdefaults['port'], 
+        SQLdefaults['login'], SQLdefaults['password'])
+    assert (isinstance(sql_transport, MySQLtransport) == True)
+
+def test_mysql_wrong_host():
+    with pytest.raises(TransportConnectionError):
+        sql_transport = MySQLtransport('_wrong_', SQLdefaults['port'], 
+            SQLdefaults['login'], SQLdefaults['password'])
+
+def test_mysql_wrong_port():
+    with pytest.raises(TransportConnectionError):
+        sql_transport = MySQLtransport(SQLdefaults['host'], -1, 
+            SQLdefaults['login'], SQLdefaults['password'])
+
+def test_mysql_wrong_user():
+    with pytest.raises(TransportConnectionError):
+        sql_transport = MySQLtransport(SQLdefaults['host'], SQLdefaults['port'], 
+            '_wrong_', SQLdefaults['password'])
+
+def test_mysql_wrong_password():
+    with pytest.raises(TransportConnectionError):
+        sql_transport = MySQLtransport(SQLdefaults['host'], SQLdefaults['port'], 
+            SQLdefaults['login'], '_wrong_')
+
+def test_get_transport():
+    sql_transport = get_transport('SQL')
+    assert (isinstance(sql_transport, MySQLtransport) == True)
+
+def test_exec():
+    prepare_base()
+    sql_transport = get_transport('SQL').sql_exec("SELECT `id`, `password` FROM `users` WHERE `email`=%s", sql_data)
+    assert (isinstance(sql_transport, dict) == True)
+
+def test_empty_exec():
+    sql_transport = get_transport('SQL').sql_exec("", "")
+    assert sql_transport is None
+
+def test_not_empty_table():
+    sql_transport = get_transport('SQL').check_if_empty_table('users')
+    assert sql_transport is False
+
+def test_empty_table():
+    drop_n_create_table()
+    sql_transport = get_transport('SQL').check_if_empty_table('users')
+    assert sql_transport is True
+
+def test_all_empty_tables():
+    prepare_base()
+    sql_transport = get_transport('SQL').all_empty_tables()
+    assert (isinstance(sql_transport, list) == True)
+
+def test_all_not_empty_tables():
+    sql_transport = get_transport('SQL').all_not_empty_tables()
+    assert (isinstance(sql_transport, list) == True)
+
+def test_check_database_exist():
+    sql_transport = get_transport('SQL').check_database_exist('def_database')
+    assert sql_transport is True
+
+def test_check_database_not_exist():
+    sql_transport = get_transport('SQL').check_database_exist('_unknown_')
+    assert sql_transport is False
---
+++ src/tests/test_sqlite.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Tests for database handling
+from db_handling import connect_database, load_json, create_db, DatabaseError, add_control, Status
+import sqlite3
+import pytest
+
+database = 'database.db'
+
+# Check if function return object of sqlite3
+def test_connect_database():
+    create_connection = connect_database()
+    is_obj_instance = isinstance(create_connection, sqlite3.Connection)
+    assert is_obj_instance is True
+
+# Check if we return list of parameters from json config
+def test_json_loader():
+    json_base = load_json()
+    is_obj_list = isinstance(json_base, list)
+    assert is_obj_list is True
+
+def test_create_database():
+    create_database = create_db()
+    assert create_database is None
+
+def test_add_control_more():
+    with pytest.raises(DatabaseError):
+        add_control(0, 10)
+
+def test_add_control_less():
+    with pytest.raises(DatabaseError):
+        add_control(0, 0)
+
+def test_add_control_good():
+    for i in range(1,5):
+        control_func = add_control(0, i)
+        assert control_func is None
