f99c88a22abcde78d1e9efe2bfbb5a7efc2a8483
user <regwebghost@yandex.ru>
2018-05-04 11:42:15 +0700

    update script for upping mariadb

--- mariadb-up.sh
+++ mariadb-up.sh
@@ -4,4 +4,3 @@ set -e
 
 docker pull mariadb
 docker run --name pt-mariadb -p 127.0.0.1:43306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=user -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=def_database --rm mariadb:latest
-docker exec -it pt-mariadb bash -c 'exec mysql -hlocalhost -P3306 -uroot -p'
