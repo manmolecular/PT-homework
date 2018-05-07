7430ea28d19a41fab6bc58811f7f9f8a62d85972
user <regwebghost@yandex.ru>
2018-05-07 22:48:37 +0700

    remove commit in context manager

--- src/tests/test_sql.py
+++ src/tests/test_sql.py
@@ -18,26 +18,23 @@ def prepare_base():
             cursorclass=pymysql.cursors.DictCursor, 
             unix_socket=False)
 
-    with connection.cursor() as cursor:
-        cursor.execute('''
-            DROP TABLE IF EXISTS users;
-            ''')
+    with connection:
+        connection.cursor().execute('''
+                DROP TABLE IF EXISTS users;
+                ''')
 
-    with connection.cursor() as cursor:
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS `users` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `email` varchar(255) COLLATE utf8_bin NOT NULL,
-        `password` varchar(255) COLLATE utf8_bin NOT NULL,
-        PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-            AUTO_INCREMENT=1 ;''')
+        connection.cursor().execute('''
+                CREATE TABLE IF NOT EXISTS `users` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `email` varchar(255) COLLATE utf8_bin NOT NULL,
+                `password` varchar(255) COLLATE utf8_bin NOT NULL,
+                PRIMARY KEY (`id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+                    AUTO_INCREMENT=1 ;''')
 
-    with connection.cursor() as cursor:
         sql = "INSERT IGNORE INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+        connection.cursor().execute(sql, ('webmaster@python.org', 'very-secret'))
 
-    connection.commit()
     connection.close()
 
 def drop_n_create_table():
@@ -50,13 +47,12 @@ def drop_n_create_table():
         cursorclass=pymysql.cursors.DictCursor, 
         unix_socket=False)
 
-    with connection.cursor() as cursor:
-        cursor.execute('''
+    with connection:
+        connection.cursor().execute('''
             DROP TABLE IF EXISTS users;
             ''')
-   
-    with connection.cursor() as cursor:
-        cursor.execute('''
+
+        connection.cursor().execute('''
         CREATE TABLE IF NOT EXISTS `users` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
         `email` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -65,7 +61,6 @@ def drop_n_create_table():
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
             AUTO_INCREMENT=1 ;''')
 
-    connection.commit()
     connection.close()
 
 def test_mysql_init():
