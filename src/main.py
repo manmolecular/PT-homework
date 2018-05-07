4649f94d37a4a7703c1aa67c0374860e8db95582
user <regwebghost@yandex.ru>
2018-05-07 21:37:07 +0700

    remake to use db_handling class

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 # Main module for scripts calling
 from pathlib import Path
 from transports import SSHtransport
-from db_handling import add_control, create_db
+from db_handling import sqlite_handle
 from report import scan_info, make_report
 import importlib
 import time
 import os
 
 _scriptdir = 'scripts'
+LOCAL_DB = sqlite_handle()
 
 # Import all scripts from folder
 def import_scripts():
@@ -17,11 +18,11 @@ def import_scripts():
         if file.name != '__init__.py':
             status = importlib.import_module('.' + file.name[:-3], package = _scriptdir).main()
             script_id = int(file.name[0:3])
-            add_control(script_id, status)
+            LOCAL_DB.add_control(script_id, status)
 
 def main():
     start_time = time.time()
-    create_db()
+    LOCAL_DB.create_db()
     import_scripts()
     end_time = time.time() - start_time
     scan_info(end_time)
