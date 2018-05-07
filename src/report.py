52a8eb92a5a7d8602daf0d5a8645d1d3f575e7be
user <regwebghost@yandex.ru>
2018-05-07 22:20:40 +0700

    turn tuples into classes

--- src/report.py
+++ src/report.py
@@ -4,8 +4,8 @@ from jinja2 import Environment, PackageLoader, FileSystemLoader, select_autoesca
 from collections import namedtuple
 from weasyprint import HTML, CSS
 from db_handling import DatabaseError, connect_database
+from typing import NamedTuple
 import datetime
-
 
 SCAN_STRUCTURE = {
     'date': None,
@@ -21,6 +21,24 @@ SCAN_STRUCTURE = {
     'transport': None
 }
 
+
+class control_script_info(NamedTuple):
+    filename: str
+    header: str
+    script_id: int
+    descr: str
+    requirement: str
+    status: str
+    transport: str
+
+
+class basic_scan_info(NamedTuple):
+    date: str
+    longitude: str
+    counter: int
+    counter_not_null: int
+
+
 def scan_info(longitude = None):
     connection = connect_database()
     curr = connection.cursor()
@@ -30,12 +48,10 @@ def scan_info(longitude = None):
     SCAN_STRUCTURE['counter'] = curr.execute("SELECT Count(*) FROM scandata").fetchall()[0][0]
     SCAN_STRUCTURE['counter_not_null'] = curr.execute("SELECT Count(*) FROM scandata WHERE status IS NOT NULL").fetchall()[0][0]
 
+
 def get_rendered_html():
     connection = connect_database()
     curr = connection.cursor()
-
-    control_script_info = namedtuple('control_script_info', 'filename, header, script_id, descr, requirement, status, transport')
-    basic_scan_info = namedtuple('basic_scan_info', 'date, longitude, counter, counter_not_null')
 
     report_data = []
     scandata_ids = curr.execute("SELECT id FROM scandata").fetchall()
@@ -70,6 +86,7 @@ def get_rendered_html():
 
     template = env.get_template('index.html')
     return template.render(report_data = report_data, scan_data = scan_data)
+
 
 def make_report():
     whtml = HTML(string=get_rendered_html().encode('utf8'))
