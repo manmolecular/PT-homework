01b3558623144180fabdbad995d04829266dd819
user <regwebghost@yandex.ru>
2018-05-08 01:00:41 +0700

    fully pep8

--- src/transports.py
+++ src/transports.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python3
 # SSH transport class based on PT-security lectures
 from get_config import get_config
-
 import paramiko
 import pymysql.cursors
 import socket
 import json
 
-# Some helpful dicts for using
-_get_file_defaults = {
-        'file_name': 'testfile',
-        'remote_path': './',
-        'local_path': './'
-    }
+FILE_DEFAULT = 'testfile'
+
 
 # Classes for error handling
 class TransportError(Exception):
     def __init__(self, error_args):
         super().__init__(self, 'TransportError {}'.format(error_args))
 
+
 class TransportUnknown(Exception):
     def __init__(self, error_args):
         Exception.__init__(self, 'TransportUnknown {}'.format(error_args))
+
 
 class TransportConnectionError(TransportError):
     def __init__(self, error_args):
         TransportError.__init__(self, error_args)
 
+
 class TransportIOError(TransportError):
     def __init__(self, error_args):
         TransportError.__init__(self, error_args)
+
 
 # MySQL transport
 class MySQLtransport():
     def __init__(self, host, port, login, password):
         try:
-            self.connection = pymysql.connect(host = host, 
-                user = login, 
-                port = port, 
-                password = password, 
-                db = 'def_database', 
-                charset='utf8', 
-                cursorclass=pymysql.cursors.DictCursor, 
+            self.connection = pymysql.connect(
+                host=host,
+                user=login,
+                port=port,
+                password=password,
+                db='def_database',
+                charset='utf8',
+                cursorclass=pymysql.cursors.DictCursor,
                 unix_socket=False)
         except pymysql.MySQLError as e:
             raise TransportConnectionError(e) from e
@@ -59,7 +59,8 @@ class MySQLtransport():
 
     def check_database_exist(self, database_name):
         with self.connection.cursor() as cursor:
-            sql_query = 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = %s'
+            sql_query = 'SELECT SCHEMA_NAME FROM \
+            INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = %s'
             cursor.execute(sql_query, database_name)
             result = cursor.fetchone()
             if result:
@@ -101,42 +102,45 @@ class MySQLtransport():
     def __del__(self):
         self.connection.close()
 
+
 # SSH transport class
 class SSHtransport():
     def __init__(self, host, port, login, password):
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            self.client.connect(hostname = host, username = login, password = password, port = port)
-        except (paramiko.BadHostKeyException, paramiko.AuthenticationException, 
-        paramiko.SSHException, socket.error) as e:
+            self.client.connect(
+                hostname=host,
+                username=login,
+                password=password,
+                port=port)
+        except (paramiko.BadHostKeyException, paramiko.AuthenticationException,
+                paramiko.SSHException, socket.error) as e:
             raise TransportConnectionError(e) from e
-            
+
     def __del__(self):
         self.client.close()
 
-    def exec(self, command = None):
+    def exec(self, command=None):
         if not command:
-            raise TransportError({'command':command})
+            raise TransportError({'command': command})
         stdin, stdout, stderr = self.client.exec_command(command)
         return stdout.read()
 
-    def get_file(self, file_name = None, remote_path = None, local_path = None):
-        file_name = file_name or _get_file_defaults['file_name']
-        remote_path = remote_path or _get_file_defaults['remote_path']
-        local_path = local_path or _get_file_defaults['local_path']
+    def get_file(self, file_name=None):
+        file_name = file_name or FILE_DEFAULT
 
         if not file_name:
-            raise TransportError({'file_name':file_name})
-        file_remote = remote_path + file_name
-        file_local = local_path + file_name
+            raise TransportError({'file_name': file_name})
         sftp = self.client.open_sftp()
+
         try:
             sftp.stat(file_remote)
         except paramiko.SSHException:
             raise TransportConnectionError('paramiko: SSHException')
         except IOError:
             raise TransportIOError('file doesnt exist')
+
         file = sftp.open(file_name, mode='r', bufsize=-1).read()
         sftp.close()
         return file
@@ -144,25 +148,28 @@ class SSHtransport():
 
 # Set default area of transport names
 global_transport_names = {
-        'SSH':SSHtransport,
-        'SQL':MySQLtransport
+        'SSH': SSHtransport,
+        'SQL': MySQLtransport
     }
+
 
 # Get defaults from config file
 def get_defaults(transport_name):
-    _json_config = get_config()
+    json_cfg = get_config()
     return {
-        'host': _json_config['host'], 
-        'port':_json_config['transports'][transport_name]['port'], 
-        'login':_json_config['transports'][transport_name]['login'], 
-        'password':_json_config['transports'][transport_name]['password']
+        'host': json_cfg['host'],
+        'port': json_cfg['transports'][transport_name]['port'],
+        'login': json_cfg['transports'][transport_name]['login'],
+        'password': json_cfg['transports'][transport_name]['password']
     }
 
+
 # Get unique transport of some class
-def get_transport(transport_name, host = None, port = None, login = None, password = None):
+def get_transport(transport_name, host=None,
+                  port=None, login=None, password=None):
     if transport_name not in global_transport_names:
-        raise TransportUnknown({'transport_name':transport_name})
-    
+        raise TransportUnknown({'transport_name': transport_name})
+
     default = get_defaults(transport_name)
     host = host or default['host']
     port = port or default['port']
