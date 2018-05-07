d0596f52989fd1b8ba8c7c7a7d78054e68523334
user <regwebghost@yandex.ru>
2018-05-07 22:04:03 +0700

    pep8 constants - main

--- src/main.py
+++ src/main.py
@@ -8,15 +8,15 @@ import importlib
 import time
 import os
 
-_scriptdir = 'scripts'
+SCRIPT_DIR = 'scripts'
 LOCAL_DB = sqlite_handle()
 
 # Import all scripts from folder
 def import_scripts():
-    script_dir = Path('./' + _scriptdir)
+    script_dir = Path('./' + SCRIPT_DIR)
     for file in script_dir.glob('**/*.py'):
         if file.name != '__init__.py':
-            status = importlib.import_module('.' + file.name[:-3], package = _scriptdir).main()
+            status = importlib.import_module('.' + file.name[:-3], package = SCRIPT_DIR).main()
             script_id = int(file.name[0:3])
             LOCAL_DB.add_control(script_id, status)
 
