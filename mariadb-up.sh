26bfdf1d3ee621b7f3b702e7b6a083e27ab763d3
user <regwebghost@yandex.ru>
2018-05-07 11:34:35 +0700

    remove old scripts

--- mariadb-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run mariadb
-set -e
-
-docker pull mariadb
-docker run --name pt-mariadb -p 127.0.0.1:43306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=user -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=def_database --rm mariadb:latest
