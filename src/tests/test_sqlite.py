aff0f44aea5cd6d58b30a256d371d3b8c2f428cf
user <regwebghost@yandex.ru>
2018-05-07 21:37:47 +0700

    update tests due to db_handling

--- src/tests/test_sqlite.py
+++ src/tests/test_sqlite.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # Tests for database handling
-from db_handling import connect_database, load_json, create_db, DatabaseError, add_control, Status
+from db_handling import connect_database, load_json, DatabaseError, sqlite_handle, Status
 import sqlite3
 import pytest
 
 database = 'database.db'
+LOCAL_DB = sqlite_handle()
 
 # Check if function return object of sqlite3
+def test_sqlite_object():
+    assert isinstance(LOCAL_DB, sqlite_handle) is True
+
 def test_connect_database():
     create_connection = connect_database()
     is_obj_instance = isinstance(create_connection, sqlite3.Connection)
@@ -19,10 +23,10 @@ def test_json_loader():
     assert is_obj_list is True
 
 def test_create_database():
-    create_database = create_db()
+    create_database = LOCAL_DB.create_db()
     assert create_database is None
 
 def test_add_control_good():
     for i in range(1,5):
-        control_func = add_control(0, i)
+        control_func = LOCAL_DB.add_control(0, i)
         assert control_func is None
