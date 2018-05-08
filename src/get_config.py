a13381c33508f6888294d70e3b9bad6ad863f271
user <regwebghost@yandex.ru>
2018-05-08 12:57:09 +0700

    fix paths in get_config

--- src/get_config.py
+++ src/get_config.py
@@ -4,12 +4,13 @@ import json
 from pathlib import Path
 
 JSON_CONFIG = None
-CONFIG_CONTEST = 'configs/config.json'
+CONFIG_CONTEST = 'config.json'
+CONFIG_DIR = 'configs'
 
 
 def get_config():
     global JSON_CONFIG
     if not JSON_CONFIG:
-        with Path('.').joinpath(CONFIG_CONTEST).open() as f:
+        with Path('.').joinpath(CONFIG_DIR).joinpath(CONFIG_CONTEST).open() as f:
             JSON_CONFIG = json.load(f)
     return JSON_CONFIG
