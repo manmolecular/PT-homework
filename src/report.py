19bafbc54120d10eca653b8e98f69acf43ae501e
user <regwebghost@yandex.ru>
2018-05-10 03:19:59 +0700

    new report structure

--- src/report.py
+++ src/report.py
@@ -19,89 +19,56 @@ SCAN_STRUCTURE = {
 }
 
 
-class control_script_info(NamedTuple):
+class control_info(NamedTuple):
+    contrid: int
+    description: str
     filename: str
-    header: str
-    script_id: int
-    descr: str
     requirement: str
-    status: str
+    name: str
     transport: str
+    status: str
 
 
 class basic_scan_info(NamedTuple):
-    scan_id: int
-    date: str
-    longitude: str
-    counter: int
-    counter_not_null: int
-    curr_test: list
-
-# Get rendered html
-# Basic idea: we make scan of system and this scan is marked with 
-# some id (for example, 1). For this scan we put all of passed controls
-# in scandata table with _same_ id (for our example, 1). 
-
-# Scandata (id, list(info about every control))
-
-# After this, when we render report, we take _system scan_
-# and search results of _controls testing_ on it.
-# On fingers: system scan id = 1, than scandata results = 1
-# That's how we do it - all information about every scan
-# will be saved.
+    scanid: int
+    scandate: str
+    start_time: str
+    end_time: str
+    duration: str
+    tests_count: int
+    not_null_status: int
+    controls: list
 
 def get_rendered_html():
     connection = connect_database()
     curr = connection.cursor()
 
+    scans = connection.execute('SELECT * FROM scansystem').fetchall()
+
     render_data = []
-    scan_count = curr.execute("SELECT id FROM scansystem").fetchall()
-
-    # Look at all full scans of our system from db 
-    for full_scan_id in scan_count:
+    for scan in scans:
+        scan_controls = []
+        scandatas = connection.execute('SELECT * FROM scandata WHERE scansystem_id = ?', (str(scan[0]))).fetchall()
+        for scandata in scandatas:
+            control = connection.execute('SELECT * FROM control WHERE id = ?', (str(scandata[5]))).fetchone()
+            scan_controls.append(control_info(
+                contrid=control[0],
+                description=control[1],
+                filename=control[2],
+                requirement=control[3],
+                name=scandata[1],
+                transport=scandata[2],
+                status=scandata[3]))
         
-        # Find all controls that we test on some full scan of system
-        # Parse string (TEXT field from db) -> list
-        this_scan_controls = ast.literal_eval(
-            curr.execute("SELECT * FROM scandata \
-            WHERE id = ?", str(full_scan_id[0])).fetchone()[1])
-
-        # List of info about every control from this sysscan
-        list_of_controls_info = []
-        
-        # For all controls from some full scan we retrieve all information
-        for curr_control in this_scan_controls:
-            from_db_info = curr.execute("SELECT * FROM control \
-                WHERE id = ?", [str(curr_control[0])]).fetchone()
-            SCAN_STRUCTURE['id'] = curr_control[0]
-            SCAN_STRUCTURE['header'] = curr_control[1]
-            SCAN_STRUCTURE['descr'] = from_db_info[1]
-            SCAN_STRUCTURE['status'] = curr_control[3]
-            SCAN_STRUCTURE['filename'] = from_db_info[2]
-            SCAN_STRUCTURE['requirements'] = from_db_info[3]
-            SCAN_STRUCTURE['transport'] = curr_control[2]
-            list_of_controls_info.append(control_script_info(
-                filename=SCAN_STRUCTURE['filename'],
-                header=SCAN_STRUCTURE['header'],
-                script_id=SCAN_STRUCTURE['id'],
-                descr=SCAN_STRUCTURE['descr'],
-                requirement=SCAN_STRUCTURE['requirements'],
-                status=SCAN_STRUCTURE['status'],
-                transport=SCAN_STRUCTURE['transport']))
-
-        # Get major debug info from scanning of system (date, time, etc.)
-        from_db_fullscan_info = curr.execute("SELECT * FROM scansystem \
-            WHERE id = ?", str(full_scan_id[0])).fetchone()
-
-        # Get list of all data that we must render to pdf 
-        # (controls info + sys info of scan)
         render_data.append(basic_scan_info(
-            scan_id=from_db_fullscan_info[0],
-            date=from_db_fullscan_info[1],
-            longitude=from_db_fullscan_info[2],
-            counter=from_db_fullscan_info[3],
-            counter_not_null=from_db_fullscan_info[4],
-            curr_test = list_of_controls_info))
+            scanid=scan[0],
+            scandate=scan[1],
+            start_time=scan[2],
+            end_time=scan[3],
+            duration=scan[4],
+            tests_count=scan[5],
+            not_null_status=scan[6],
+            controls=scan_controls))
 
     connection.close()
 
