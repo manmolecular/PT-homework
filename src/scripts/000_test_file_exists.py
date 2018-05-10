857de0bf44acd6726ab50bc3f31d53a1fbeaf471
user <regwebghost@yandex.ru>
2018-05-10 13:46:11 +0700

    update control scripts

--- src/scripts/000_test_file_exists.py
+++ src/scripts/000_test_file_exists.py
@@ -5,18 +5,14 @@ from db_handling import Status
 
 FILE_NAME = 'testfile'
 TRANSPORT = 'SSH'
-INFO_COL = []
 
 def main():
     try:
         transport_instance = get_transport(TRANSPORT)
         func_status = transport_instance.get_file(FILE_NAME)
-        transport_name = transport_instance.__class__.__name__
-        INFO_COL.append(transport_name)
     except TransportUnknown:
-        return [TRANSPORT, Status.STATUS_ERROR]
+        return Status.STATUS_ERROR
     if func_status:
-        INFO_COL.append(Status.STATUS_COMPLIANT)
+        return Status.STATUS_COMPLIANT
     else:
-        INFO_COL.append(Status.STATUS_NOT_COMPLIANT)
-    return INFO_COL
+        return Status.STATUS_NOT_COMPLIANT
