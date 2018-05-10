e02f18c2a853e05bfc92931e46dbfe959a69fd92
user <regwebghost@yandex.ru>
2018-05-11 00:57:44 +0700

    update sqlite tests

--- src/tests/test_sqlite.py
+++ src/tests/test_sqlite.py
@@ -7,6 +7,7 @@ import ast
 import pytest
 
 local_db = sqlite_handle()
+local_db.create_db()
 
 # Check if function return object of sqlite3
 def test_sqlite_object():
@@ -17,16 +18,42 @@ def test_connect_database():
 
 # Check if we return list of parameters from json config
 def test_json_loader():
+    connection = connect_database()
     assert isinstance(load_json(), list)
 
+    controls = connection.execute(
+            'SELECT * from control').fetchall()
+
+    for index, json in enumerate(load_json()):
+        assert (int(json[0]) == int(controls[index][0]))
+        for i in range(1, 4):
+            assert (str(json[i]) == str(controls[index][i]))
+
+    connection.close()
+
 def test_create_database():
-    create_database = local_db.create_db()
     connection = connect_database()
+
     with connection:
         assert isinstance(connection.execute(
             'SELECT * from control').fetchall(), list)
         assert isinstance(connection.execute(
             'SELECT * from control').fetchall(), list)
+    
+    control = ['id', 'description', 'filename', 'requirement', 'transport']
+    scandata = ['id', 'name', 'transport', 'status', 'scansystem_id', 'control_id']
+    scansystem = ['id', 'scandate', 'start_time', 'end_time', 'duration', 'tests_count', 'not_null_status']
+
+    control_columns = connection.execute("PRAGMA table_info(control);").fetchall()
+    scandata_columns = connection.execute("PRAGMA table_info(scandata);").fetchall()
+    scansystem_columns = connection.execute("PRAGMA table_info(scansystem);").fetchall()
+    for index, controldb in enumerate(control_columns):
+        assert(controldb[1] == control[index])
+    for index, scandatadb in enumerate(scandata_columns):
+        assert(scandatadb[1] == scandata[index])
+    for index, scansystemdb in enumerate(scansystem_columns):
+        assert(scansystemdb[1] == scansystem[index])
+
     connection.close()
 
 def test_add_control_good():
