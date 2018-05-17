a843803d72815d24e2f84a2a26df477ce453fb39
manmolecular <regwebghost@yandex.ru>
2018-05-18 03:12:50 +0700

    new WMI scripts

---
+++ src/scripts/005_EnableVirtualization.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from transports import WMIregistryTransport, get_transport, TransportUnknown
+from db_handling import Status
+
+TRANSPORT = 'WMIreg'
+
+
+def main():
+    try:
+        reg =  get_transport(TRANSPORT)
+        result = reg.get_value(
+            'Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System',
+            'EnableVirtualization')
+    except TransportUnknown:
+        return Status.STATUS_ERROR
+    if bool(result):
+        return Status.STATUS_COMPLIANT
+    else:
+        return Status.STATUS_NOT_COMPLIANT
