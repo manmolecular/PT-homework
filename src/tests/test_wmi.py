31802156bb724338c3a0078cad78030873a43b64
manmolecular <regwebghost@yandex.ru>
2018-05-23 02:06:03 +0700

    update wmi test with fixtures

--- src/tests/test_wmi.py
+++ src/tests/test_wmi.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
 import wmi
 
+import pytest
+
 from transports import get_transport
 
 
-def setup_module():
-    global WMIconnect
-    global WMIregistry
-    WMIconnect = get_transport('WMI')
-    WMIregistry = get_transport('WMIreg')
+@pytest.fixture
+def WMIconnect():
+    return get_transport('WMI')
+
+@pytest.fixture
+def WMIregistry():
+    return get_transport('WMIreg')
 
 
-def test_valid_command_status():
+def test_valid_command_status(WMIconnect):
     assert (bool(WMIconnect.wmi_exec('ipconfig')['result']) == False)
 
 
-def test_return_object_wmi_query():
+def test_return_object_wmi_query(WMIconnect):
     result = WMIconnect.wmi_query("Select Name, \
             DNSHostName, Domain, Workgroup, PartOfDomain \
             from Win32_ComputerSystem")[0]
     assert isinstance(result, wmi._wmi_object)
 
 
-def test_registry_get_value():
+def test_registry_get_value(WMIregistry):
     result = WMIregistry.get_value(
         'Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System',
         'EnableLUA')
