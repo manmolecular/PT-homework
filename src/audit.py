e124b11c0e8e606b3e92fe2a6be0ad7cd860429c
manmolecular <regwebghost@yandex.ru>
2018-05-24 21:38:06 +0700

    update audit

--- src/audit.py
+++ src/audit.py
@@ -1,19 +1,26 @@
 from transports import get_transport, SNMPtransport
 import re
 
-connection = get_transport('SNMP')
-sysDescr = connection.get_snmpdata('.1.3.6.1.2.1.1.1.0')
-for value in sysDescr:
-    print(value)
+def SNMP_audit():
+    connection = get_transport('SNMP')
+    sysDescr = connection.get_snmpdata('.1.3.6.1.2.1.1.1.0')
+    for value in sysDescr:
+        print(value)
 
-interfacesQuantity = connection.get_snmpdata('.1.3.6.1.2.1.2.1.0')[0]
-listOfInterfaces = []
-for interface in range(1, interfacesQuantity):
-    listOfInterfaces.append([
-        ((connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.2.'+ str(interface))[0]).prettyPrint()),
-        (connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.7.'+ str(interface))[0].prettyPrint())
-    ])
+    interfacesQuantity = connection.get_snmpdata('.1.3.6.1.2.1.2.1.0')[0]
+    listOfInterfaces = []
+    for interface in range(1, interfacesQuantity):
+        listOfInterfaces.append([
+            ((connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.2.'+ str(interface))[0]).prettyPrint()),
+            (connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.7.'+ str(interface))[0].prettyPrint())
+        ])
 
-listOfInterfaces.sort()
-for interface in listOfInterfaces:
-    print (interface)
+    listOfInterfaces.sort()
+    for interface in listOfInterfaces:
+        print (interface)
+
+def SSH_audit():
+    connection = get_transport('SSH')
+    print(connection.exec('uname -a'))
+
+SSH_audit()
