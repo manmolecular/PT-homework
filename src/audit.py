c52ae3847b8b800095e4db1ac92198804d94bc1a
manmolecular <regwebghost@yandex.ru>
2018-05-25 01:07:50 +0700

    try-except blocks for errors

--- src/audit.py
+++ src/audit.py
@@ -1,17 +1,22 @@
-from transports import get_transport, SNMPtransport
+from transports import get_transport, SNMPtransport, TransportError
 import re
 
 
 def SNMP_audit():
     connection = get_transport('SNMP')
-    sysDescr = connection.get_snmpdata('.1.3.6.1.2.1.1.1.0')
-    interfacesQuantity = connection.get_snmpdata('.1.3.6.1.2.1.2.1.0')[0]
-    listOfInterfaces = []
-    for interface in range(1, interfacesQuantity):
-        listOfInterfaces.append([
-            ((connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.2.'+ str(interface))[0]).prettyPrint()),
-            (connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.7.'+ str(interface))[0].prettyPrint())
-        ])
+    try:
+        sysDescr = connection.get_snmpdata('.1.3.6.1.2.1.1.1.0')
+        if isinstance(sysDescr, pysnmp.proto.errind.RequestTimedOut):
+            print('oh hi')
+        interfacesQuantity = connection.get_snmpdata('.1.3.6.1.2.1.2.1.0')[0]
+        listOfInterfaces = []
+        for interface in range(1, interfacesQuantity):
+            listOfInterfaces.append([
+                ((connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.2.'+ str(interface))[0]).prettyPrint()),
+                (connection.get_snmpdata('.1.3.6.1.2.1.2.2.1.7.'+ str(interface))[0].prettyPrint())
+            ])
+    except Exception:
+        return None
 
     listOfInterfaces.sort()
 
@@ -23,7 +28,11 @@ def SNMP_audit():
 
 
 def SSH_audit():
-    connection = get_transport('SSH')
+    try:
+        connection = get_transport('SSH')
+    except TransportError:
+        print('Warning: SSH service is unavailable')
+        return None
     SSH_audit_info = {
         'sys_info': connection.exec('uname -a'),
         'cpu_info': connection.exec('lscpu'),
@@ -36,4 +45,10 @@ def SSH_audit():
 
 
 def retrieve_audit_info():
-    return [SNMP_audit(), SSH_audit()]
+    print('Getting SNMP and SSH system info...')
+    SNMP = SNMP_audit()
+    SSH = SSH_audit()
+    if ((SNMP == None) and (SSH == None)):
+        return None
+    else:
+        return [SNMP, SSH]
