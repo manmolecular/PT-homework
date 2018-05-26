886ef67be868e122e7e7acae1d48a3a558a6a095
manmolecular <regwebghost@yandex.ru>
2018-05-26 13:28:41 +0700

    work on hackathon pdf

---
+++ src/hackathon.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from weasyprint import HTML, CSS
+
+from ssh_info import get_ssh_info
+
+def get_rendered_html():
+    render_data = get_ssh_info()
+
+    env = Environment(
+        loader=FileSystemLoader('hackathon_report'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('index.html')
+    return template.render(scan_data=render_data)
+
+def make_hackathon_report():
+    whtml = HTML(string=get_rendered_html().encode('utf8'))
+    wcss = CSS(filename='./templates/style.css')
+    whtml.write_pdf('hackathon.pdf', stylesheets=[wcss])
+
+make_hackathon_report()
